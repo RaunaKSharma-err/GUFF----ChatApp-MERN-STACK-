27d503e99efd211da7b5c331c00ea97a32850815
RaunaKSharma-err <progritik@gmail.com>
2025-03-04 09:41:41 +0545

    deployement update

--- client/src/store/useThemeStore.tsx
+++ client/src/store/useThemeStore.tsx
@@ -6,7 +6,7 @@ type themeStore = {
 };
 
 export const useThemeStore = create<themeStore>((set) => ({
-  theme: localStorage.getItem("chat-theme") || "light",
+  theme: localStorage.getItem("chat-theme") || "dark",
 
   setTheme: (theme: string) => {
     localStorage.setItem("chat-theme", theme);
