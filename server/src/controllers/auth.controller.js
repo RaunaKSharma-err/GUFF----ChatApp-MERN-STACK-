bb1f68f1324d17d7ac8830edcb817092a2e3da12
RaunaKSharma-err <progritik@gmail.com>
2025-03-04 09:49:42 +0545

    auth changes

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -61,6 +61,11 @@ export const login = async (req, res) => {
       return res.status(400).json({ message: "user not found" });
     }
 
+    const isPasswordCorrect = await bycrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
     generateToken(user._id, res);
 
     res.status(200).json({
